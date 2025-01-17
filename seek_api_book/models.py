59294df6eeace5e726ef522aa27ab8fe206a7697
Sebastian Betancourth <s.betancourth@uniandes.edu.co>
2025-01-17 10:43:25 -0500

    feat:Configuraciòn de modelo y base de datos para mongobd

--- seek_api_book/models.py
+++ seek_api_book/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.conf import settings
+from bson.objectid import ObjectId
 
-# Create your models here.
+class Book:
+    collection = settings.MONGO_DB["books"]
+
+    @staticmethod
+    def create(data):
+        """Crea un libro en la colección."""
+        return Book.collection.insert_one(data)
+
+    @staticmethod
+    def get_all():
+        """Obtiene todos los libros."""
+        return list(Book.collection.find())
+
+    @staticmethod
+    def get_by_id(book_id):
+        """Obtiene un libro por su ID."""
+        return Book.collection.find_one({"_id": ObjectId(book_id)})
+
+    @staticmethod
+    def update(book_id, data):
+        """Actualiza un libro por su ID."""
+        return Book.collection.update_one({"_id": ObjectId(book_id)}, {"$set": data})
+
+    @staticmethod
+    def delete(book_id):
+        """Elimina un libro por su ID."""
+        return Book.collection.delete_one({"_id": ObjectId(book_id)})
+
+
--- seek_book/settings.py
+++ seek_book/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from pymongo import MongoClient
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,13 +77,9 @@ WSGI_APPLICATION = 'seek_book.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+MONGO_CLIENT = MongoClient("mongodb://localhost:27017/")
+MONGO_DB = MONGO_CLIENT["library"]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
